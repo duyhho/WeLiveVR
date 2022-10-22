a38bdd4bf97198e7877bc8aeb3331fbff096e7b6
Duy Ho <dhh3hb@mail.umkc.edu>
2022-10-22 15:02:44 -0500

    Initial commit

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public void OnButtonPressed() {
+        Debug.Log("This is pressed!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Unity_VR_Training
+HackARoo VR Training
+
+Sample VR Project Tested with Oculus Quest 2
+
+Unity Version: 2020.3.13f
+
+Required Platform: Android
+
+Required Framework: XR Interaction Toolkit
